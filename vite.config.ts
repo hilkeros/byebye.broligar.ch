4ede6549cb0e619233d5a036daf0db8fa7fd88c0
Hilke Ros <>
2026-06-01 17:16:02 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { hatk } from "@hatk/hatk/vite-plugin";
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  plugins: [hatk(), sveltekit()],
+  test: {
+    include: ["test/**/*.test.ts"],
+    exclude: ["test/browser/**"],
+  },
+  lint: {
+    ignorePatterns: ["hatk.generated.ts", "hatk.generated.client.ts"],
+  },
+  fmt: {
+    ignorePatterns: ["hatk.generated.ts", "hatk.generated.client.ts"],
+  },
+});
